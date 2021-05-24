f1a5ff9538a65e44f343f20f2745e548763d2a3c
MysteryCoder456 <rehatbir.singh@gmail.com>
2021-05-24 20:12:55 +0400

    add and remove cells

--- LIFE.cpp
+++ LIFE.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 
 #define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
@@ -16,14 +17,53 @@ public:
 	}
 
 private:
+	olc::TileTransformedView tv;
+	std::vector<olc::vi2d> vCells;
+
+private:
 	bool OnUserCreate() override
 	{
+		tv = olc::TileTransformedView({ ScreenWidth(), ScreenHeight() }, { 16, 16 });
 		return true;
 	}
 
 	bool OnUserUpdate(float fElapsedTime) override
 	{
+		// Handle pan and zoom
+		if (GetMouse(2).bPressed) tv.StartPan(GetMousePos());
+		if (GetMouse(2).bHeld) tv.UpdatePan(GetMousePos());
+		if (GetMouse(2).bReleased) tv.EndPan(GetMousePos());
+		if (GetMouseWheel() > 0) tv.ZoomAtScreenPos(1.5f, GetMousePos());
+		if (GetMouseWheel() < 0) tv.ZoomAtScreenPos(0.6f, GetMousePos());
+
+		// Placing cells
+		if (GetMouse(0).bHeld)
+		{
+			olc::vi2d vClickPos = tv.ScreenToWorld(GetMousePos());
+
+			if (std::find(vCells.begin(), vCells.end(), vClickPos) == vCells.end())
+				vCells.push_back(vClickPos);
+		}
+		else if (GetMouse(1).bHeld)
+		{
+			olc::vi2d vClickPos = tv.ScreenToWorld(GetMousePos());
+
+			if (std::find(vCells.begin(), vCells.end(), vClickPos) != vCells.end())
+				std::remove(vCells.begin(), vCells.end(), vClickPos);
+		}
+
+		for (int i = 0; i < vCells.size(); i++)
+		{
+			// Decide the fate of each cell...
+		}
+
 		Clear(olc::DARK_BLUE);
+
+		for (olc::vi2d& cell : vCells)
+		{
+			tv.FillRect(cell, { 1, 1 }, olc::WHITE);
+		}
+
 		return true;
 	}
 };
