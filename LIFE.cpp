60fa7282a56cc0330d9ee10424dc7031b342e2eb
MysteryCoder456 <rehatbir.singh@gmail.com>
2021-05-24 14:43:30 +0400

    pixel game engine stuff

--- LIFE.cpp
+++ LIFE.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
 
+#define OLC_PGE_APPLICATION
+#include "olcPixelGameEngine.h"
+
+#define OLC_PGEX_TRANSFORMEDVIEW
+#include "olcPGEX_TransformedView.h"
+
+
+class GameOfLife : public olc::PixelGameEngine
+{
+public:
+	GameOfLife()
+	{
+		sAppName = "Conway's Game of Life";
+	}
+
+private:
+	bool OnUserCreate() override
+	{
+		return true;
+	}
+
+	bool OnUserUpdate(float fElapsedTime) override
+	{
+		Clear(olc::DARK_BLUE);
+		return true;
+	}
+};
+
+
 int main(int argc, char **argv)
 {
-	std::cout << "Conway's Game Of Life";
+	GameOfLife game;
+	if (game.Construct(1024, 720, 1, 1))
+		game.Start();
+
 	return 0;
 }
