8cd3d9d4aa06329212c0f6b2f431684a9321da6c
MysteryCoder456 <rehatbir.singh@gmail.com>
2021-05-25 13:20:39 +0400

    semi broken cell fate

--- LIFE.cpp
+++ LIFE.cpp
@@ -23,8 +23,12 @@ private:
 	bool *bCells = new bool [nTotalCells];
 
 private:
+	bool bPause = true;
+
+private:
 	void ClearMap()
 	{
+		std::cout << "Clearing map...\n";
 		for (int i = 0; i < nTotalCells; i++)
 			bCells[i] = false;
 	}
@@ -55,20 +59,60 @@ private:
 
 		// Keyboard controls
 		if (GetKey(olc::C).bPressed)
-		{
-			std::cout << "Clearing map...\n";
 			ClearMap();
+		if (GetKey(olc::P).bPressed)
+			bPause = !bPause;
+
+		if (!bPause)
+		{
+			// Create a temporary map
+			bool *bTempCells = new bool [nTotalCells];
+			for (int i = 0; i < nTotalCells; i++)
+				bTempCells[i] = false;
+
+			// Run calculate the fate of each cell without modifying original map
+			for (int y = 0; y < vWorldSize.y; y++)
+			{
+				for (int x = 0; x < vWorldSize.x; x++)
+				{
+					int index = y * vWorldSize.x + x;
+					int TLIndex = (y - 1) * vWorldSize.x + (x - 1);
+					int TMIndex = (y - 1) * vWorldSize.x + x;
+					int TRIndex = (y - 1) * vWorldSize.x + (x + 1);
+					int MLIndex = y * vWorldSize.x + (x - 1);
+					int MRIndex = y * vWorldSize.x + (x + 1);
+					int BLIndex = (y + 1) * vWorldSize.x + (x - 1);
+					int BMIndex = (y + 1) * vWorldSize.x + x;
+					int BRIndex = (y + 1) * vWorldSize.x + (x + 1);
+
+					int nNeighbourCount =
+							bCells[TLIndex] +
+							bCells[TMIndex] +
+							bCells[TRIndex] +
+							bCells[MLIndex] +
+							bCells[MRIndex] +
+							bCells[BLIndex] +
+							bCells[BMIndex] +
+							bCells[BRIndex];
+
+					bool bCellAlive = bCells[index];
+					if (bCellAlive)
+					{
+						if (nNeighbourCount < 2 || nNeighbourCount > 3)
+							bTempCells[index] = false;
+					}
+					else if (nNeighbourCount == 3)
+						bTempCells[index] = true;
+				}
+			}
+
+			// Set actual map to temporary map
+			bCells = bTempCells;
 		}
 
-//		for (int y = 0; y < vWorldSize.y; y++)
-//		{
-//			for (int x = 0; x < vWorldSize.x; x++)
-//			{
-//				// Decide the fate of each cell...
-//			}
-//		}
-
 		Clear(olc::DARK_BLUE);
+		if (bPause)
+			DrawString(0, 0, "Paused!");
 
 		for (float y = 0; y < vWorldSize.y; y++)
 		{
